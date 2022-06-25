55e8126ec3df8baed62b76e56ec649af8178df31
nehiro <hiroaki.nerio@gmail.com>
2022-06-25 14:38:08 +0900

    update state.ts for deploy

--- lib/state.ts
+++
@@ -1,8 +0,0 @@
-// export const createState = functions
-//   .region('asia-northeast1')
-//   .https.onCall(async (data, context) => {
-//     // ランダム文字列を生成
-//     const state: string = admin.firestore().collection('_').doc().id;
-//     await admin.firestore().doc(`states/${state}`).set({ state });
-//     return state;
-//   });

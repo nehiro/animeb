6d5fcf80bdd91418f0262bf698c0853a0ccc46c8
nehiro <hiroaki.nerio@gmail.com>
2022-06-28 19:29:05 +0900

    update with windows

--- firebase/server.ts
+++ firebase/server.ts
@@ -2,16 +2,14 @@ import { initializeApp, cert, getApps, App } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 // console.log(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string));
-
 if (!getApps()?.length) {
   initializeApp({
     credential: cert(
       // 環境変数から認証情報を取得
-      JSON.parse(
-        process.env.NEXT_PUBLIC_PROD === 'true'
-          ? (process.env.GOOGLE_APPLICATION as string)
-          : (process.env.GOOGLE_APPLICATION_DEV as string)
-      )
+      JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS as string)
+      // process.env.NEXT_PUBLIC_PROD === 'true'
+      //     ? (process.env.GOOGLE_APPLICATION as string)
+      //     : (process.env.GOOGLE_APPLICATION_DEV as string)
     ),
   });
 }

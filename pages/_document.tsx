9206e5812deca92e055709338676f6eb52349d7d
nehiro <hiroaki.nerio@gmail.com>
2022-06-28 16:56:13 +0900

    update with windows

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -44,10 +44,17 @@ class MyDocument extends Document {
             </>
           ) : (
             <>
+              {/* Chrome / Firefox / Edge */}
               <link
                 rel="icon"
-                href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>⚠️</text></svg>"
+                href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🚧</text></svg>"
               ></link>
+              {/* Safari / IE */}
+              <link
+                rel="icon alternate"
+                type="image/png"
+                href="https://twemoji.maxcdn.com/v/14.0.2/72x72/1f6a7.png"
+              />
               {/* <link
                 rel="apple-touch-icon"
                 sizes="180x180"

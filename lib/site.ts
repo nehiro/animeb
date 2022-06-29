12e507163f66ec9060f0c6d3a3358ec0351a6355
nehiro <hiroaki.nerio@gmail.com>
2022-06-29 14:46:06 +0900

    update with windows

--- lib/site.ts
+++ lib/site.ts
@@ -3,13 +3,17 @@ export const Site = () => {
   //   process.env.NEXT_PUBLIC_PROD,
   //   'process.env.NEXT_PUBLIC_PROD SITE'
   // );
-  return { origin: 'https://anime-club.online/' };
-  // return { origin: 'http://localhost:3000/' };
-  // if (process.env.NEXT_PUBLIC_PROD === 'true') {
-  //   // console.log('本番');
-  //   return { origin: 'https://anime-club.online/' };
-  // } else {
-  //   // console.log('開発');
-  //   return { origin: 'http://localhost:3000' };
-  // }
+  // return { origin: 'https://anime-club.online/' };
+  // return { origin: 'https://anime-club.online/' };
+  if (process.env.NEXT_PUBLIC_PROD === 'true') {
+    // console.log('本番');
+    return { origin: 'https://anime-club.online' };
+  } else {
+    // console.log('開発');
+    return { origin: 'http://localhost:3000' };
+  }
 };
+// export const Site = {
+//   // origin: 'https://anime-club.online'
+//   origin: 'http://localhost:3000',
+// };

e0cba8694aa73d252cb0529ba960408677c53d2f
nehiro <hiroaki.nerio@gmail.com>
2022-07-06 22:27:14 +0900

    update with mac

--- pages/api/send-mail.ts
+++ pages/api/send-mail.ts
@@ -6,26 +6,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { to, name, text, html, contents } = req.body;
   // console.log(to, '送ってきた人');
-  //user
-  const msg: MailDataRequired = {
-    to,
-    from: 'contact@anime-club.online',
-    subject: '【アニメ部！】お問合せありがとうございました。',
-    templateId: 'd-46898b6820a84a5db8840dd33e792d67',
-    dynamicTemplateData: {
-      name,
-      contents,
-    },
-  };
 
-  try {
-    await sgMail.send(msg);
-    res
-      .status(200)
-      .send('メール送信に成功しました。\n内容を確認次第、ご返信いたします。');
-  } catch (error) {
-    res.status(500).send('メール送信に失敗しました。');
-  }
   //admin
   const msgToAdmin: MailDataRequired = {
     to: 'contact@anime-club.online',
@@ -48,6 +29,26 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   } catch (error) {
     console.log(error, 'admin失敗');
     // res.status(500).send('メール送信に失敗しました。');
+  }
+  //user
+  const msg: MailDataRequired = {
+    to,
+    from: 'contact@anime-club.online',
+    subject: '【アニメ部！】お問合せありがとうございました。',
+    templateId: 'd-46898b6820a84a5db8840dd33e792d67',
+    dynamicTemplateData: {
+      name,
+      contents,
+    },
+  };
+
+  try {
+    await sgMail.send(msg);
+    res
+      .status(200)
+      .send('メール送信に成功しました。\n内容を確認次第、ご返信いたします。');
+  } catch (error) {
+    res.status(500).send('メール送信に失敗しました。');
   }
 };
 
